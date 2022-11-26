e5f08ca80bb49ca59686e242441ebb25912f6830
Oksana Dushar <dushar0937@gmail.com>
2022-11-26 17:50:05 +0100

    Homework 2
    - static int
    - package creation

---
+++ src/de/telran/dushar/oksana/game/hero/Constant.java
@@ -0,0 +1,7 @@
+package de.telran.dushar.oksana.game.hero;
+
+public class Constant {
+    static int GNOME_POWER = 5;
+    static int GNOME_CHARISMA = 1;
+    static int GNOME_MAGIC = 2;
+}
---
+++ src/de/telran/dushar/oksana/game/hero/Gnome.java
@@ -0,0 +1,10 @@
+package de.telran.dushar.oksana.game.hero;
+
+public class Gnome {
+    int id;
+    int age;
+    int endurance;
+    int intelligence;
+    boolean isGrouchy;
+    int fightSkill;
+}
